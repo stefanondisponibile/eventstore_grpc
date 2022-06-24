7f5067ed3302dabab72d9cbd8e610700d3ac3a11
stefano <frassetto.stefano@gmail.com>
2022-06-24 23:03:14 +0200

    Minor changes

--- src/eventstore_grpc/core/__init__.py
+++ src/eventstore_grpc/core/__init__.py
@@ -1,1 +1,3 @@
 from .client_base import ClientBase
+from .auth import Auth
+from .transport import Transport
--- tests/core/test_settings.py
+++
@@ -1,7 +0,0 @@
-from eventstore_grpc.core.settings import Node
-
-
-class TestNode:
-    def test_get_url(self) -> None:
-        n = Node(host="foo.com", port=8080, scheme="http")
-        assert n.get_url() == "http://foo.com:8080"
