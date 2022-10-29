4952d2f0d7801a6817d5e8925563afea7032af66
stefanondisponibile <frassetto.stefano@gmail.com>
2022-10-29 17:38:37 +0200

    refactor: cleaner client and some todos

--- src/eventstore_grpc/client/event_store.py
+++ src/eventstore_grpc/client/event_store.py
@@ -16,17 +16,34 @@ from eventstore_grpc.core.auth import Auth
 
 
 class EventStore(ClientBase):
+    """EventStore Client."""
+
     def __init__(
         self,
         hosts: Union[str, list[str]],
-        discover: Optional[bool] = None,
-        tls: Optional[bool] = None,
-        keep_alive_interval: Optional[int] = None,
-        keep_alive_timeout: Optional[int] = None,
+        discover: Optional[bool] = None,  # TODO: maybe remove?
+        tls: Optional[bool] = None,  # TODO: maybe remove and keep just tls_ca_file.
+        keep_alive_interval: Optional[int] = None,  # TODO: do we need this?
+        keep_alive_timeout: Optional[int] = None,  # TODO: do we need this?
         username: Optional[str] = None,
         password: Optional[str] = None,
         tls_ca_file: Optional[str] = None,
-    ):
+    ) -> None:
+        """Initializes a new EventStore Client.
+
+        Args:
+            hosts: either a single host url, or multiple ones, as a list.
+            discover: whether or not to use discovery.
+            tls: whether or not to use tls encryption.
+            keep_alive_interval: the number of seconds for the keep alive interval.
+            keep_alive_timeout: the number of seconds for the keep alive timeout.
+            username: the username.
+            password: the password.
+            tls_ca_file: the filepath to the certificate to use for tls encryption.
+
+        Raises:
+            ValueError
+        """
         if not any([keep_alive_interval, keep_alive_timeout]):
             keep_alive = None
         else:
@@ -39,11 +56,10 @@ class EventStore(ClientBase):
         if not tls and tls_ca_file:
             raise ValueError(f"tls is {tls} but you specified a ca_file: {tls_ca_file}")
 
-        auth = Auth(root_certificate=tls_ca_file)
-
-        if all([username, password]):
-            auth.username = username
-            auth.password = password
+        if any([username, password]) and not all([username, password]):
+            raise ValueError(
+                "You must specify both username AND password, or none of them."
+            )
 
         super().__init__(
             Transport(
@@ -51,7 +67,9 @@ class EventStore(ClientBase):
                 discover=discover,
                 tls=tls,
                 keep_alive=keep_alive,
-                auth=auth,
+                auth=Auth(
+                    root_certificate=tls_ca_file, username=username, password=password
+                ),
             )
         )
         self.gossip = Gossip(self._transport)
@@ -60,5 +78,5 @@ class EventStore(ClientBase):
         self.projections = Projections(self._transport)
         self.subscriptions = Subscriptions(self._transport)
         self.streams = Streams(self._transport)
-        if self._transport.tls:
+        if self._transport.tls:  # TODO: is this the right condition?
             self.users = Users(self._transport)
