193d1672c52cb7f85070edf6c3665589fa833bf0
stefanondisponibile <frassetto.stefano@gmail.com>
2022-11-01 09:38:08 +0100

    refactor: remove unused comments

--- src/eventstore_grpc/client/__init__.py
+++
@@ -1,8 +0,0 @@
-# from eventstore_grpc.client.event_store import EventStore
-# from .gossip import Gossip
-# from .operations import Operations
-# from .persistent import Persistent
-# from .projections import Projections
-# from .streams import Streams
-# from .subscriptions import Subscriptions
-# from .users import Users
